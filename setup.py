bab952d81dc8a8f5d40b484fb977b99c7702c12f
Jonas Witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2022-07-21 09:24:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version="0.3.4",
     description="Cloud and Virga detection based on radar reflectivity and ceilometer cloud base height.",
     url="https://github.com/remsens-lim/virga_sniffer.git",
-    license="GPLv3",
+    license="GPL-3.0",
     author="Jonas Witthuhn",
     author_email="remsensarctic@uni-leipzig.de",
     packages=["virga_sniffer"],
