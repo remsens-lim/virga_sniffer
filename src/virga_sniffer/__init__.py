a3462c40457ebed40495836319c644d72f3af593
jonas-witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2025-03-18 09:09:43 +0100

    switch to pyproject.toml

--- src/virga_sniffer/__init__.py
+++ src/virga_sniffer/__init__.py
@@ -1,6 +1,14 @@
 from .virga_detection import virga_mask
 from .layer_utils import process_cbh
 
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("virga_sniffer")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+
 __all__ = (
     # Top-level functions
     "virga_mask",
